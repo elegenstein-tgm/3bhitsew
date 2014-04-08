2d7e7ea4dcdce33d5bb6f5da7e4d0174f93433f9
ErikBrändli <elegenstein@student.tgm.ac.at>
2014-04-08 13:51:50 +0200

    Crypt added

---
+++ JavaVerschlüsselung/src/Cipher.java
@@ -0,0 +1,6 @@
+
+public interface Cipher {
+	
+	public String encrypt(String text);
+	public String decrypt(String text);
+}
---
+++ JavaVerschlüsselung/src/SubstitutionCipher.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author Erik
+ *
+ */
+public class SubstitutionCipher extends MonoAlpabethicCipher{
+	public SubstitutionCipher(String secretAlphabet){
+		this.setSecretAlphabet(secretAlphabet);
+	}
+	public void setSecretAlphabet(String secretAlphabet){
+		this.setSecretAlphabet(secretAlphabet);
+	}
+}
